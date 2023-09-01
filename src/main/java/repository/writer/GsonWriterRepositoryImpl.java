94576817c23c982bb651550711d8a42e5caf510d
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-09-01 15:23:50 +0300

    Added generic read-write to/from a file methods for the utility class

--- src/main/java/repository/writer/GsonWriterRepositoryImpl.java
+++ src/main/java/repository/writer/GsonWriterRepositoryImpl.java
@@ -5,16 +5,11 @@ import lombok.AllArgsConstructor;
 import model.Status;
 import model.Writer;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static util.Util.generateNewId;
+import static util.Util.*;
 
 @AllArgsConstructor
 public class GsonWriterRepositoryImpl implements WriterRepository {
@@ -24,27 +19,29 @@ public class GsonWriterRepositoryImpl implements WriterRepository {
 
     @Override
     public Writer getById(String id) throws IOException {
-        return getAll().stream().filter(writer -> writer.getId().equals(id)).findAny().orElseThrow();
+        return getListFromFile(gson, fileName, Writer.class).stream()
+                .filter(writer -> writer.getId().equals(id))
+                .findAny()
+                .orElseThrow();
     }
 
     @Override
     public List<Writer> getAll() throws IOException {
-        Writer[] writers = gson.fromJson(Files.newBufferedReader(Paths.get(fileName)), Writer[].class);
-        return writers != null ? new ArrayList<>(Arrays.asList(writers)) : new ArrayList<>();
+        return getListFromFile(gson, fileName, Writer.class);
     }
 
     @Override
     public Writer save(Writer writer) throws IOException {
         writer.setId(generateNewId());
-        List<Writer> writerList = getAll();
+        List<Writer> writerList = getListFromFile(gson, fileName, Writer.class);
         writerList.add(writer);
-        saveToFile(writerList);
+        saveToFile(writerList, gson, fileName);
         return writer;
     }
 
     @Override
     public Writer update(Writer updatedWriter) throws IOException {
-        List<Writer> writerList = getAll().stream()
+        List<Writer> writerList = getListFromFile(gson, fileName, Writer.class).stream()
                 .map(writer -> {
                     if (writer.getId().equals(updatedWriter.getId())) {
                         return updatedWriter;
@@ -52,24 +49,18 @@ public class GsonWriterRepositoryImpl implements WriterRepository {
                     return writer;
                 })
                 .collect(Collectors.toList());
-        saveToFile(writerList);
+        saveToFile(writerList, gson, fileName);
         return updatedWriter;
     }
 
     @Override
     public void deleteById(String id) throws IOException {
-        List<Writer> writerList = getAll().stream()
+        List<Writer> writerList = getListFromFile(gson, fileName, Writer.class).stream()
                 .peek(writer -> {
                     if (writer.getId().equals(id)) {
                         writer.setStatus(Status.DELETED);
                     }
                 }).collect(Collectors.toList());
-        saveToFile(writerList);
-    }
-
-    private void saveToFile(List<Writer> writerList) throws IOException {
-        try (FileWriter fileWriter = new FileWriter(fileName)){
-            gson.toJson(writerList, fileWriter);
-        }
+        saveToFile(writerList, gson, fileName);
     }
 }
--- src/main/java/util/Util.java
+++ src/main/java/util/Util.java
@@ -1,15 +1,43 @@
 package util;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonParser;
 import lombok.experimental.UtilityClass;
+import model.BaseEntity;
 
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 @UtilityClass
 public class Util {
-    public static String generateNewId(){
+    public static String generateNewId() {
         int randomInt = ThreadLocalRandom.current().nextInt(1, 1000 + 1);
         int currentTimeNano = LocalDateTime.now().getNano();
         return randomInt + "" + currentTimeNano;
+    }
+
+    public static <T extends BaseEntity> List<T> getListFromFile(Gson gson, String fileName, Class<T> clazz) throws IOException {
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(fileName))) {
+            JsonArray jsonArray = JsonParser.parseReader(reader).getAsJsonArray();
+            return jsonArray.asList().stream()
+                    .map(jsonElement -> gson.fromJson(jsonElement, clazz))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    public static <T extends BaseEntity> void saveToFile(List<T> list, Gson gson, String fileName) throws IOException {
+        try (FileWriter fileWriter = new FileWriter(fileName)) {
+            gson.toJson(list, fileWriter);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
