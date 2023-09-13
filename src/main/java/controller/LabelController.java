e15c570db3a8ba769fc6fb40fdf2e4e198f2e683
Alexey Boyarinov <boyarinovalexey@gmail.com>
2023-09-13 22:13:18 +0300

    Added BaseController

--- src/main/java/controller/LabelController.java
+++ src/main/java/controller/LabelController.java
@@ -1,34 +1,11 @@
 package controller;
 
-import lombok.AllArgsConstructor;
 import model.Label;
-import repository.label.LabelRepository;
+import repository.GenericRepository;
 
-import java.io.IOException;
-import java.util.List;
+public class LabelController extends BaseController<Label, String>{
 
-@AllArgsConstructor
-public class LabelController {
-
-    LabelRepository labelRepository;
-
-    public Label getById(String id) throws IOException {
-        return labelRepository.getById(id);
-    }
-
-    public List<Label> getAll() throws IOException {
-        return labelRepository.getAll();
-    }
-
-    public Label save(Label label) throws IOException {
-        return labelRepository.save(label);
-    }
-
-    public Label update(Label label) throws IOException {
-        return labelRepository.update(label);
-    }
-
-    public void deleteById(String id) throws IOException {
-        labelRepository.deleteById(id);
+    public LabelController(GenericRepository <Label, String> repository) {
+        super(repository);
     }
 }
--- src/main/java/controller/PostController.java
+++ src/main/java/controller/PostController.java
@@ -1,38 +1,22 @@
 package controller;
 
-import lombok.AllArgsConstructor;
 import model.Post;
+import repository.GenericRepository;
 import repository.post.PostRepository;
 
 import java.io.IOException;
 import java.util.List;
 
-@AllArgsConstructor
-public class PostController {
+public class PostController extends BaseController<Post, String>{
 
     PostRepository postRepository;
 
-    public Post getById(String id) throws IOException {
-        return postRepository.getById(id);
-    }
-
-    public List<Post> getAll() throws IOException {
-        return postRepository.getAll();
+    public PostController(GenericRepository<Post, String> repository) {
+        super(repository);
     }
 
     public List<Post> getAllByWriterId(String writerId) throws IOException {
         return postRepository.getAllByWriterId(writerId);
     }
 
-    public Post save(Post post) throws IOException {
-        return postRepository.save(post);
-    }
-
-    public Post update(Post post) throws IOException {
-        return postRepository.update(post);
-    }
-
-    public void deleteById(String id) throws IOException {
-        postRepository.deleteById(id);
-    }
 }
